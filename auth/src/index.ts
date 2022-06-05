6590fb0999c84d5fb7ee7119ecd6800ac33a5f69
kimkevin90 <65535673+kimkevin90@users.noreply.github.com>
2022-06-05 14:07:40 +0900

    [micro] 테스트를 위한 index & app 분리

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-// ingress nginx 트래픽 신뢰 위함
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  }),
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res, next) => {
-  // next(new NotFoundError());
-  // express-async-errors 설치 후
-  throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from './app';
 
 const startApp = async () => {
   if (!process.env.JWT_KEY) {
