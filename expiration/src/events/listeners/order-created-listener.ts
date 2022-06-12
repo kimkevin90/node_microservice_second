3e0dbfae49d032b5753a6529b85d5b4b641b39b6
kimkevin90 <65535673+kimkevin90@users.noreply.github.com>
2022-06-12 20:38:48 +0900

    [micro] create-expiration-complete-event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,5 +1,6 @@
 import { Listener, OrderCreatedEvent, Subjects } from '@jsk8stickets/common';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queue/expiration-queue';
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
@@ -10,14 +11,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
     console.log('Waiting this many milliseconds to process the job:', delay);
 
-    // await expirationQueue.add(
-    //   {
-    //     orderId: data.id,
-    //   },
-    //   {
-    //     delay,
-    //   }
-    // );
+    // 15분 후  expirationQueue.process에 orderId 전달
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: 5000,
+      },
+    );
 
     msg.ack();
   }
